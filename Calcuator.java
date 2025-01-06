42fd9c7899c8d099aa301d94872146fb0f640e9b
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-06 23:43:47 +0530

    add the functionality of multiply

--- Calcuator.java
+++ Calcuator.java
@@ -16,6 +16,12 @@ public static double subtract(double a, double b) {
 }
 
 
+// Method to multiply two numbers
+public static double multiply(double a, double b) {
+    return a * b;
+}
+
+
     public static void main(String[] args) {
         System.out.println("welcome to my calculator::");
         Scanner sc = new Scanner(System.in);
