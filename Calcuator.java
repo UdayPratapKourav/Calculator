0ec6dbcbc690569a3dc5175ddf8f3994e9af9bb6
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-06 23:32:41 +0530

    add Subtract functionality

--- Calcuator.java
+++ Calcuator.java
@@ -10,6 +10,11 @@ public class Calcuator {
     return a + b;
 }
 
+//Method to substract two numbers
+public static double subtract(double a, double b) {
+    return a - b;
+}
+
 
     public static void main(String[] args) {
         System.out.println("welcome to my calculator::");
