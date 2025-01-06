adc9bab72eda34a770e80db1e960d11190e9c4e5
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-06 23:13:21 +0530

    basic code

--- Calcuator.java
+++ Calcuator.java
@@ -1,7 +1,28 @@
 package Calculator;
 
+import java.util.Scanner;
+
 public class Calcuator {
     public static void main(String[] args) {
         System.out.println("welcome to my calculator::");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose an operation:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = sc.nextInt();
+
+        double result;
+
+        
     }
 }
